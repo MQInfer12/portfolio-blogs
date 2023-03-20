573122efc30805adcff7f9bdd106e088e83959a4
MQInfer12 <maummq@gmail.com>
2023-03-20 15:02:02 -0400

    Removing base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import react from "@astrojs/react";
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://MQInfer12.github.io',
-  base: '/',
-  output: 'server',
-  adapter: node({
-    mode: "standalone"
-  })
+  site: 'https://MQInfer12.github.io'
 });
