7bf2667760cbaa4cf2d44ad06e384cb105a87f94
MQInfer12 <maummq@gmail.com>
2023-03-20 14:29:09 -0400

    Output server and standalone

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
-
+import node from '@astrojs/node';
 import react from "@astrojs/react";
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   site: 'https://MQInfer12.github.io',
-  base: '/',  
+  base: '/',
+  output: 'server',
+  adapter: node({
+    mode: "standalone"
+  })
 });
