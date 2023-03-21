0a00e371df980b1ce467100d48adb0065fc89d34
MQInfer12 <maummq@gmail.com>
2023-03-20 21:28:44 -0400

    First section and navbar responsive

---
+++ src/components/layouts/Navbar/Hamburguer/Hamburguer.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import './Hamburguer.scss';
+
+const Hamburguer = (props) => {
+  const [active, setActive] = useState(false);
+
+  return (
+    <div className="hamburguer">
+      <input id="navham" onChange={() => setActive(old => !old)} type="checkbox" />
+      <label for="navham" className={active ? "fa-solid fa-xmark" : "fa-solid fa-bars"}></label>
+      <div className="navbar-positioner">
+        { props.links }
+      </div>
+    </div>
+  )
+}
+
+export default Hamburguer
